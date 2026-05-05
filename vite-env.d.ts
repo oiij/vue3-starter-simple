1aa8a5cb99a5a9f4bf25f097b718b529ce58f076
antrioe <antrioe@gmali.com>
2026-05-05 17:58:08 +0800

    ci(deps): :ferris_wheel: ci

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -17,13 +17,4 @@ declare module '*.md' {
   const Component: ComponentOptions
   export default Component
 }
-// 声明 vite 环境变量
-declare interface ImportMetaEnv {
-  readonly VITE_APP_NAME: string
-  readonly VITE_APP_DESCRIPTION: string
-  readonly VITE_BASE: string
-  readonly VITE_API_BASE_PREFIX: string
-  readonly VITE_API_BASE_URL: string
-  readonly VITE_DEV_PORT: number
-  // 更多环境变量...
-}
+ 
